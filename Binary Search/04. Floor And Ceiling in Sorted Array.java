966827a17e872938d78a3f1a778bb0accabc5021
Aviral Sharma <101351283+aviralsharma07@users.noreply.github.com>
2023-07-23 12:42:18 +0530

    Update 04. Floor And Ceiling in Sorted Array.java

--- Binary Search/04. Floor And Ceiling in Sorted Array.java
+++ Binary Search/04. Floor And Ceiling in Sorted Array.java
@@ -4,23 +4,7 @@ public class FloorAndCeiling {
       Problem Link: https://www.codingninjas.com/studio/problems/ceiling-in-a-sorted-array_1825401
       Ceiling = Lower Bound
   */
-    static int floor(int[] arr, int target){
-        int n = arr.length;
-        int start = 0;
-        int end = n - 1;
-        int floor = n;
-        while (start <= end){
-            int mid = start + (end - start)/2;
-            if (arr[mid] >= target){
-                floor = arr[mid];
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return floor;
-    }
-
+  
     static int ceiling(int[] arr, int target){
         int n = arr.length;
         int start = 0;
@@ -28,14 +12,31 @@ public class FloorAndCeiling {
         int ceiling = n;
         while (start <= end){
             int mid = start + (end - start)/2;
-            if (arr[mid] <= target){
+            if (arr[mid] >= target){
                 ceiling = arr[mid];
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ceiling;
+    }
+
+    static int floor(int[] arr, int target){
+        int n = arr.length;
+        int start = 0;
+        int end = n - 1;
+        int floor = n;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (arr[mid] <= target){
+                floor = arr[mid];
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return ceiling;
+        return floor;
     }
 
     public static void main(String[] args) {
