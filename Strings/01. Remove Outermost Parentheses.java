382208669d76d1e1de15fe8942e3dce660e74071
Aviral Sharma <101351283+aviralsharma07@users.noreply.github.com>
2024-02-01 16:54:30 +0530

    Create 01. Remove Outermost Parentheses.java

---
+++ Strings/01. Remove Outermost Parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+      
+      // CREATE A STACK TO KEEP TRACK OF THE PARENTHESES
+        Stack<Character> stack = new Stack<>();
+
+      // A VARIABLE TO KEEP TRACK OF OPEN PARENTHESES
+        int openCount = 0;
+
+      // AN EMPTY STRING TO STORE THE RESULT 
+        String answer = "";
+        for (char c: s.toCharArray()){
+            if (c == '('){
+                if (openCount > 0){
+                  // Add '(' to the result only if it's not the outermost '('
+                    answer += c;
+                }
+                stack.push(c);
+                openCount++;
+            } else if (c == ')') {
+                stack.pop();
+                openCount--;
+                if (openCount > 0){
+                  // Add ')' to the result only if it's not the outermost ')'
+                    answer += c;
+                }
+            }
+        }
+        return answer;
+    }
+}
