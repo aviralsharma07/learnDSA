48eb3720dee4aad28f53bd28296b8adb67212195
Aviral Sharma <101351283+aviralsharma07@users.noreply.github.com>
2024-03-19 19:16:52 +0530

    Create 15. Delete all occurrences of a given key in a doubly linked list.java

---
+++ Linked List/15. Delete all occurrences of a given key in a doubly linked list.java
@@ -0,0 +1,34 @@
+// Problem Link: https://www.codingninjas.com/studio/problems/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list_8160461
+
+public class Solution {
+    public static Node deleteAllOccurrences(Node head, int k) {
+        if(head == null) return head;
+        Node current = head;
+        while (current != null){
+            if (current.data == k) {
+                head = deleteNode(head, current);
+            }
+            current = current.next;
+        }
+        return head;
+    }
+
+    public static Node deleteNode(Node head, Node current){
+        Node back = current.prev;
+        Node front = current.next;
+
+        if (front == null && back == null){
+            head = null;
+        }else if(back == null){
+            head.next.prev = null;
+            head = head.next;
+        }else if(front == null){
+            current.prev.next = null;
+        }else{
+            back.next = front;
+            front.prev = back;
+        }
+
+        return head;
+    }
+}
